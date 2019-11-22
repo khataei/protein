669b05f2495faaa8114c77cd0d1b5eb30e605b23
Javad-mun <skhataeipour@mun.ca>
2019-11-21 23:16:49 -0330

    Read Todo first. Several SVMs are added.

--- Reg-models.R
+++ Reg-models.R
@@ -6,11 +6,14 @@
 library(tidyverse)
 library(data.table)
 library(caret)
+library(magrittr)
 
-
+# Run this or split later
 cv_folds <- 10
 fitControl <-
   trainControl(method = "cv", number = cv_folds, classProbs = TRUE)
+#########
+
 
 working_df <- fread("Case-one.csv")
 
@@ -19,7 +22,61 @@ working_df$Kcat_Km <- round(x = log(working_df$Kcat_Km,0.2)*2)/2
 
 working_df <- working_df %>%  filter(Kcat_Km != Inf)
 
+classes <- c("A","B","C", "D", "E" , "F", "G" , "H", "I" , "J", "K", "L")
+working_df$Kcat_Km %<>% factor(labels = classes)
 
-lm_fit <- train(Kcat_Km ~ .,
+
+# Fit models
+# SVM
+SVM_fit <- train(Kcat_Km ~ .,
                 data = working_df, 
-                method = "blasso")
+                method = "svmLinear3")
+
+print(SVM_fit)
+
+# Bayesian Generalized Linear Model
+BGL_fit <- train(Kcat_Km ~ .,
+                 data = working_df, 
+                 method = "bayesglm")
+
+print(BGL_fit)
+
+# SVM 1
+SVM1 <- caret::train(
+  Kcat_Km ~ .,
+  data = working_df,
+  trControl = fitControl,
+  tuneGrid = data.frame(cost = 0.01,
+                        Loss = 0.1),
+  method = "svmLinear3"
+)
+
+# SVM 2
+
+SVM2 <- caret::train(
+  Kcat_Km ~ .,
+  data = working_df,
+  trControl = fitControl,
+  tuneGrid = data.frame(degree = 2, scale  = 0.1, tau = 0.1),
+  method = "lssvmPoly"
+)
+
+# SVM without caret package
+
+training_indices <-
+  createDataPartition(working_df$Kcat_Km,
+                      p = 0.8,
+                      list = FALSE)
+## train a support vector machine
+filter <- ksvm(Kcat_Km ~ ., data=working_df, kernel="rbfdot",
+               kpar=list(sigma=0.05),C=5,cross=3)
+
+
+# RF 1
+caret::train(
+  Kcat_Km ~ .,
+  data = working_df,
+  trControl = fitControl,
+  tuneGrid = data.frame(mtry = 3),
+  method = "ORFsvm"
+)
